456fddc16dc580628e51a14520bec78ccbe0aa4f
cliffmax85 <cliffmax85@gmail.com>
2022-01-28 12:05:48 -0800

    animals rednering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { useState } from 'react';
+import AnimalList from './AnimalList';
 
 function App() {
+  const [animalArray, setAnimalArray] = useState(['snek', 'cow']);
+
   return (
     <div className="App">
+      <AnimalList animals = {animalArray}/>
+      <div className='buttons'>
+        <button onClick={() => setAnimalArray([...animalArray, 'snek'])}>Snek</button>
+        <button onClick={() => setAnimalArray([...animalArray, 't-rex'])}>T-Rex</button>
+        <button onClick={() => setAnimalArray([...animalArray, 'cow'])}>Cow</button>
+        <button onClick={() => setAnimalArray([...animalArray, 'ladybug'])}>Ladybug</button>
+      </div>
       
     </div>
   );
