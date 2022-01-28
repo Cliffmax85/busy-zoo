f30a21b30505046ea845d4522000e238cb55906a
cliffmax85 <cliffmax85@gmail.com>
2022-01-28 12:49:20 -0800

    added lion and unicorn fight

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import AnimalList from './AnimalList';
 
 function App() {
   const [animalArray, setAnimalArray] = useState(['snek', 'cow']);
+  const [lionSize, setLionSize] = useState(10);
+  const [unicornSize, setUnicornSize] = useState(10);
 
   return (
     <div className="App">
+      <div className='fight'>
+        <div className='unicorn'>
+          <p style={{ fontSize: `${unicornSize}rem` }}>🦄</p>
+          <button onClick={() => setUnicornSize(unicornSize + 1)}>Unicorns Always win</button>
+          <button onClick={() => setLionSize(lionSize - 1)}>The Unicorn Attacks!</button>
+        </div>
+        <div className='lion'>
+          <p style={{ fontSize: `${lionSize}rem` }}>🦁</p>
+          <button onClick={() => setLionSize(lionSize + 1)}>The lion feasts!</button>
+          <button onClick={() => setUnicornSize(unicornSize - 1)}>The lion bites!</button>
+        </div>
+      </div>
       <AnimalList animals = {animalArray}/>
       <div className='buttons'>
         <button onClick={() => setAnimalArray([...animalArray, 'snek'])}>Snek</button>
